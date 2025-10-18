c2efe668fa8506419d18c3cd0c677a32acc4801f
sebastian <sebastianmuellerreallife@gmail.com>
2025-10-18 14:13:55 +0200

    lang, stats and bug fixes

--- src/main/java/com/sebastian/heartbreaker_pvp/translations/GermanyColognianLanguage.java
+++ src/main/java/com/sebastian/heartbreaker_pvp/translations/GermanyColognianLanguage.java
@@ -7,64 +7,64 @@ import java.util.HashMap;
 public class GermanyColognianLanguage extends Language {
     @Override
     public String getCode() {
-        return "ksh";
+        return "ksh"; // Ändere den Code für Kölsch
     }
 
     @Override
     public String getName() {
-        return "Kölsch (Kölle)";
+        return "Kölsch (Düütschland/Rhingland)";
     }
 
     @Override
     protected HashMap<String, String> getTranslations() {
         HashMap<String, String> translations = new HashMap<>();
-        translations.put("player_not_found_or_not_whitelisted", "Dä Spiller wood nit jefonge or es nit op dä Erlaubelis!");
-        translations.put("stats_for_player", "Statistike för %s");
-        translations.put("close", "<red><b>Zoh maache</b></red>");
+        translations.put("player_not_found_or_not_whitelisted", "Spiller nit jefunge oder nit op de Wiet-Lis!");
+        translations.put("stats_for_player", "Statistik vum %s");
+        translations.put("close", "<red><b>Zomache</b></red>");
 
-        translations.put("current_health", StatsGui.Utils.translateTitle("Jäzje Jesundheit"));
+        translations.put("current_health", StatsGui.Utils.translateTitle("Aktuell Jesundheit"));
         translations.put("current_health.desc", """
                 <color:#8a9fff>%s</color><white> sing Jesundheit</white>
                 <white><green>»</green> %s/%s <gray>Punkte</gray></white>
-                <white><green>»</green> %s <gray>Absorpsjohn</gray></white>
+                <white><green>»</green> %s <gray>Absorption</gray></white>
                 """);
 
-        translations.put("hero_hearts", StatsGui.Utils.translateTitle("Jäzje Heldehätze"));
+        translations.put("hero_hearts", StatsGui.Utils.translateTitle("Aktuell Hero-Hätze"));
         translations.put("hero_hearts.desc", """
-                <color:#8a9fff>%s</color><white> sing Heldehätze</white>
-                <white><green>»</green> %s/3 <gray>Heldehätze</gray></white>
+                <color:#8a9fff>%s</color><white> sing Hero-Hätze</white>
+                <white><green>»</green> %s/3 <gray>Hero-Hätze</gray></white>
                 """);
 
-        translations.put("kills", StatsGui.Utils.translateTitle("Umgemaat"));
+        translations.put("kills", StatsGui.Utils.translateTitle("Plattjemaate"));
         translations.put("kills.desc", """
-                <color:#8a9fff>%s</color><white> hät bis jäz</white>
-                <white><green>»</green> %s <gray>Jesamt</gray></white>
+                <color:#8a9fff>%s</color><white> sing Plattjemaate bes jetz</white>
+                <white><green>»</green> %s <gray>Alls en allem</gray></white>
                 <white><green>»</green> %s <gray>Spiller</gray></white>
                 """);
 
-        translations.put("experience", StatsGui.Utils.translateTitle("Erfahrong"));
+        translations.put("experience", StatsGui.Utils.translateTitle("Aktuell Erfahrunge"));
         translations.put("experience.desc", """
-                <color:#8a9fff>%s</color><white> sing Erfahrong</white>
-                <white><green>»</green> %s <gray>Levvel</gray></white>
-                <white><green>»</green> %s%% <gray>jesamme för et nächste Levvel</gray></white>
+                <color:#8a9fff>%s</color><white> sing Erfahrunge</white>
+                <white><green>»</green> %s <gray>Level</gray></white>
+                <white><green>»</green> %s% <gray>Jesammelt för et nächste Level</gray></white>
                 """);
 
-        translations.put("deaths", StatsGui.Utils.translateTitle("Jestorve"));
+        translations.put("deaths", StatsGui.Utils.translateTitle("Jestroove"));
         translations.put("deaths.desc", """
-                <color:#8a9fff>%s</color><white> es bis jäz %s Mol jestorve</white>
-                <white><green>»</green> %s <gray>Tode</gray></white>
+                <color:#8a9fff>%s</color><white> sing Zall vun Jestroove bes jetz</white>
+                <white><green>»</green> %s <gray>Jestroove</gray></white>
                 """);
 
-        translations.put("blocks_placed", StatsGui.Utils.translateTitle("Jesetzte Klötzje"));
+        translations.put("blocks_placed", StatsGui.Utils.translateTitle("Jelaate Blöck"));
         translations.put("blocks_placed.desc", """
-                <color:#8a9fff>%s</color><white> hät bis jäz %s Klötzje jesatz</white>
-                <white><green>»</green> %s <gray>Klötzje</gray></white>
+                <color:#8a9fff>%s</color><white> sing jelaate Blöck bes jetz</white>
+                <white><green>»</green> %s <gray>Blöck</gray></white>
                 """);
 
-        translations.put("blocks_destroyed", StatsGui.Utils.translateTitle("Kapottjemaat Klötzje"));
+        translations.put("blocks_destroyed", StatsGui.Utils.translateTitle("Kapottjemaate Blöck"));
         translations.put("blocks_destroyed.desc", """
-                <color:#8a9fff>%s</color><white> hät bis jäz %s Klötzje kapott jemaat</white>
-                <white><green>»</green> %s <gray>Klötzje</gray></white>
+                <color:#8a9fff>%s</color><white> sing kapottjemaate Blöck bes jetz</white>
+                <white><green>»</green> %s <gray>Blöck</gray></white>
                 """);
 
         return translations;
